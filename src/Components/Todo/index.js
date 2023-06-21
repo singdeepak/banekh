df1d3e368d47b01b3c40996317c2caff6bcc8be7
Deepak Singh <singhdeepak84088@gmail.com>
2023-06-21 20:39:06 +0530

    First Version

--- src/Components/Todo/index.js
+++ src/Components/Todo/index.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import './style.css';
 import { Button } from 'react-bootstrap';
 
-export default function Todo(){
+export default function Todo() {
     const [activity, setActivity] = useState("");
     const [listData, setListData] = useState([]);
     const handleInput = e => setActivity(e.target.value);
@@ -15,7 +15,7 @@ export default function Todo(){
     };
     const handleRemove = (id) => {
         const newList = listData.filter((elem, ind) => {
-            return id!=ind;
+            return id != ind;
         })
         setListData(newList);
     }
@@ -23,26 +23,30 @@ export default function Todo(){
     const handleRemoveAll = () => {
         setListData([]);
     }
-    return(
+    return (
         <>
-            <div className="container">
-                <h1 className="title">Todo List</h1>
-                <input type="text" placeholder='Add activity...' value={activity} onChange={handleInput}/>
-                <Button className="addbtn" onClick={addActivity}>Add</Button>
-                <h2 className="list-title">Here is your list : {')'}</h2>
-                <ul>
-                    {listData != [] && listData.map((val, ind) => {
-                        return (
-                            <>
-                                <li key={ind}>
-                                    <b>{val}</b>
-                                    <Button className="removeBtn" onClick={()=>handleRemove(ind)}>Remove</Button>
-                                </li> 
-                            </>
-                        )
-                    })}
-                </ul>
-                {listData.length > 0 && <Button onClick={handleRemoveAll} className='removeAll'>Remove All</Button>}
+            <div className="container bg-success">
+                <div className="row">
+                    <div className="col-sm-12">
+                        <h1 className="title text-center m-3 text-warning bg-danger p-3">Todo List</h1>
+                        <input type="text" placeholder='Add activity...' value={activity} onChange={handleInput} className="form-control" />
+                        <Button className="addbtn m-5" onClick={addActivity}>Add</Button>
+                        <h2 className="list-title text-center text-warning">Here is your list : {')'}</h2>
+                        <ul>
+                            {listData != [] && listData.map((val, ind) => {
+                                return (
+                                    <>
+                                        <li key={ind} className='text-center bg-warning m-2'>
+                                            <b className="text-light">{val}</b>
+                                            <Button className="removeBtn m-3" onClick={() => handleRemove(ind)}>Remove</Button>
+                                        </li>
+                                    </>
+                                )
+                            })}
+                        </ul>
+                        {listData.length > 0 && <Button onClick={handleRemoveAll} className='removeAll m-5 bg-danger'>Remove All</Button>}
+                    </div>
+                </div>
             </div>
         </>
     )
